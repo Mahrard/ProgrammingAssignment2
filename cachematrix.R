acc25a23422fa4877c89fad4915a7f559b24bd82
Dany Majard <sevem_junkielei@hotmail.com>
2017-09-26 11:05:39 +0200

    D.Majard's assignment 26-9-2017

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function creates an environment with cached values
+## for a matrix and its inverse and creates mutator and 
+## accessor methods for them.
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  # Initiating the inverse as non existing
+  I <- NULL
+  
+  # Mutator - resets the environment with new matrix
+  set <- function(y) {
+    x <<- y
+    I <<- NULL
+  }
+  
+  # Accessor - returns the matrix
+  get <- function() x
+  
+  # Mutator - Computes new inverse
+  setinverse <- function(solve) I <<- solve
+  
+  # Accessor - Returns 'cached' inverse
+  getinverse <- function() I
+  
+  # Sets the methods of the 'MakeCacheMatrix' object
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function takes a 'MakeCacheMatrix' object and 
+## returns the inverse of the cached matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Checks if the cached matrix is invertible
+  if(det(x$get())!=0){
+    
+    ## Returns the cached inverse if already computed
+    I <- x$getinverse()
+    if(!is.null(I)) {
+      message("getting cached data")
+      return(I)
+    }
+    
+    ## otherwise computes it, sets it and returns it
+    data <- x$get()
+    I <- solve(data, ...)
+    x$setinverse(I)
+    I
+  } 
+  ## Returns error message for non invertible matrices
+  else {
+    message("Matrix non invertible")
+  }
 }
